0d5a4c4956fbbf924b1fe77faa1550f83d8613c4
Studio <harryjoji08@gmail.com>
2026-02-17 11:54:38 +0530

    Update help-setup.js

--- commands/help-setup.js
+++ commands/help-setup.js
@@ -21,7 +21,7 @@ export default {
         `Hey 👋, how can we help you?\n\n` +
         `> **This help panel can provide you with basic information about the community and how things work here.**`
       )
-      .setImage("IMAGE_LINK_MAIN") // <-- replace
+      .setImage("https://cdn.discordapp.com/attachments/1375355821688492103/1472480419365519508/0F663E29-23D8-44D7-B9F3-F57C501B4BCB.jpg?ex=6994b401&is=69936281&hm=d4743381fe6ca891a47f594cf4e7b2afadcbcfc1a836b00ac17e9f765fff3e51&") // <-- replace
       .setTimestamp();
 
     // BUTTONS
